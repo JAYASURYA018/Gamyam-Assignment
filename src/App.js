62700bbb79ee9fea07a57a00fae9c50d3c1c4416
Jayasurya <96234368+JAYASURYA018@users.noreply.github.com>
2024-12-18 12:36:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ const App = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    // Simulating API call
     fetch(
       "https://prod-be.1acre.in/lands/?division=24&page_size=10&page=2&ordering=-updated_at"
-    ) // Replace with your actual API endpoint
+    ) 
       .then((res) => res.json())
       .then((json) => setData(json))
       .catch((error) => console.error("Error fetching data:", error));
