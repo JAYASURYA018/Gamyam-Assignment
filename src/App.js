016e376e9fe2e09f4dc3c2799566ebbf755e52df
Jayasurya <96234368+JAYASURYA018@users.noreply.github.com>
2024-12-18 12:37:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import React, { useState, useEffect } from "react";
 import LandDetails from "./components/LandDetails";
 
 const App = () => {
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetch(
-      "https://prod-be.1acre.in/lands/?division=24&page_size=10&page=2&ordering=-updated_at"
-    ) 
-      .then((res) => res.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
 
   return (
     <div className="App">
