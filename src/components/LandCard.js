25ab185d7d08ac64a55631dec28157a8eb6baef5
Jayasurya <96234368+JAYASURYA018@users.noreply.github.com>
2024-12-18 12:40:49 +0530

    Update LandCard.js

--- src/components/LandCard.js
+++ src/components/LandCard.js
@@ -2,10 +2,7 @@ import React, { forwardRef } from "react";
 
 const LandCard = forwardRef(({ land }, ref) => {
   return (
-    <div className="land-card" ref={ref}>
-      <h3>{land.name}</h3>
-      <p>{land.description}</p>
-    </div>
+   <div> LandCard</div>
   );
 });
 
